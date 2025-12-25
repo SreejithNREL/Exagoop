f444b8ab48eec6f3f6037c317d50dbe0f18e9c18
SreejithNREL <sreejith.nadakkalappukuttan@nrel.gov>
2025-12-25 09:02:34 -0700

    some more changes

---
+++ Tests/1D_Axial_Bar_Vibration/PostProcess/plot_energy.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sys import argv
+
+data=np.loadtxt('./Diagnostics/Total_Energies.dat',skiprows=1)
+fig = plt.figure(1)
+ax = fig.add_subplot(111)
+ax.grid('on')
+
+plt.plot(data[:,1],data[:,2],label='TKE',color='r')
+plt.plot(data[:,1],data[:,3],label='TSE',color='blue')
+plt.plot(data[:,1],data[:,4],label='TE',color='black')
+plt.xlabel("Time ")
+plt.ylabel("Energy ")
+lgd = ax.legend()  
+# saving the file.Make sure you 
+# use savefig() before show().
+plt.savefig(argv[1])
+
