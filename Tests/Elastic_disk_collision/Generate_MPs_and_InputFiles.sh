5a96ba66fe12e7ea61c668f00da04c50c27b92ad
SreejithNREL <Sreejith.NadakkalAppukuttan@nrel.gov>
2026-01-20 22:28:59 -0700

    updated generate mp script

--- Tests/Elastic_disk_collision/Generate_MPs_and_InputFiles.sh
+++ Tests/Elastic_disk_collision/Generate_MPs_and_InputFiles.sh
@@ -3,7 +3,7 @@ python3 ./PreProcess/generate_particle_and_inputfiles.py \
     --no_of_cell_in_x 20 \
     --np_per_cell_x 1 \
     --alpha_pic_flip 1.00 \
-    --order_scheme 2 \
+    --order_scheme 3 \
     --stress_update_scheme 1 \
     --CFL 0.1 \
-    --output_tag Elastic_disk_collision__dim3_npcx1_ord2_sus1_73ab3c
+    --output_tag Elastic_disk_collision__dim3_npcx1_ord3_sus1_173ad2
