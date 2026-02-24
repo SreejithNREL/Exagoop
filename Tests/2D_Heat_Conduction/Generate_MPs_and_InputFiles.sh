dfc5a3e23b3ff2cb96784cb5b4c11298fdb05f43
SreejithNREL <sreejith.nadakkalappukuttan@nrel.gov>
2026-02-23 18:06:46 -0700

    included changes in make.local for kestrel

--- Tests/2D_Heat_Conduction/Generate_MPs_and_InputFiles.sh
+++ Tests/2D_Heat_Conduction/Generate_MPs_and_InputFiles.sh
@@ -1,1 +1,1 @@
-/opt/homebrew/Caskroom/miniforge/base/bin/python3 ./PreProcess/Generate_MPs_Inputfile_Generic.py --config ./Preprocess/config.json
+python3 ./PreProcess/Generate_MPs_Inputfile_Generic.py --config ./Preprocess/config.json
